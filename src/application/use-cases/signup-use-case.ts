6e309a17c2ef35063078aaa335a917f067138e20
TSCTiago <tiagodasilvacar@gmail.com>
2025-01-11 16:07:08 -0300

    feat: implements SignupUseCase

---
+++ src/application/use-cases/signup-use-case.ts
@@ -0,0 +1,30 @@
+import { Account } from '@/domain/entities'
+import { AccountRepository } from '@/infra/repositories'
+
+type SignupRequest = {
+  name: string
+  email: string
+  cpf: string
+  password: string
+  isPassenger: boolean
+  isDriver: boolean
+  carPlate?: string
+}
+
+export class SignupUseCase {
+  constructor(private readonly accountRepository: AccountRepository) {}
+
+  public async execute(request: SignupRequest): Promise<Account> {
+    const acount = new Account(
+      request.name,
+      request.email,
+      request.cpf,
+      request.password,
+      request.isPassenger,
+      request.isDriver,
+      request.carPlate,
+    )
+
+    return this.accountRepository.save(acount)
+  }
+}
