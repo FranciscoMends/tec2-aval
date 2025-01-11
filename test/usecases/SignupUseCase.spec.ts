684e881d8ca931318e452f8d53c91170c73c45f9
FranciscoMends <fmmagalhaesfilho@aluno.uespi.br>
2025-01-11 17:36:29 -0300

    feat: test create account driver with valid data

--- test/usecases/SignupUseCase.spec.ts
+++ test/usecases/SignupUseCase.spec.ts
@@ -38,4 +38,42 @@ describe('SignupUseCase', () => {
     expect(accountRepository.save).toHaveBeenCalledWith(expect.objectContaining(passager));
 
   });
+
+  it('should create a driver account with valid data', async () => {
+
+    let accountRepository: MockProxy<AccountRepository>
+    let signupUseCase: SignupUseCase;
+
+    accountRepository = mock<AccountRepository>();
+    signupUseCase = new SignupUseCase(accountRepository);
+
+    const driver = {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      cpf: '072.099.700-37',
+      password: 'SecurePass123',
+      isPassenger: false,
+      isDriver: true,
+      carPlate: 'ABC-1234'
+    };
+
+    const createdAccount = new Account(
+      driver.name,
+      driver.email,
+      driver.cpf,
+      driver.password,
+      driver.isPassenger,
+      driver.isDriver,
+      driver.carPlate
+    )
+
+    accountRepository.save.mockResolvedValue(createdAccount);
+    const result = await signupUseCase.execute(driver);
+
+    expect(result).toEqual(createdAccount);
+    expect(accountRepository.save).toHaveBeenCalledWith(expect.objectContaining(driver));
+
+  });
+
+
 });
